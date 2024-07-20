c3e9386f2c44db9f1b8eda4431c6e990d4319843
myoun <aiden080605@gmail.com>
2024-07-20 17:48:38 +0900

    feat: fixed available recipe

--- saveplate/main.py
+++ saveplate/main.py
@@ -5,7 +5,7 @@ from saveplate.database import ManagedTransaction, transactional
 from saveplate.util import lru_with_ttl
 from typing import Literal
 from pydantic_settings import BaseSettings
-from model import AvailableRecipeRequest
+from saveplate.model import AvailableRecipeRequest
 
 class Settings(BaseSettings):
     DB_URL: str
@@ -24,6 +24,12 @@ async def lifespan(app: FastAPI):
     database.close()
 
 app = FastAPI(lifespan=lifespan)
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    print(await request.body())
+    response = await call_next(request)
+    return response
 
 AutoCompletionType = Literal["ingredient"] | Literal["sauce"]
 
@@ -44,21 +50,20 @@ def autocompletion(tx: ManagedTransaction, type: AutoCompletionType, data: str, 
     return names
 
 @app.post("/recipes/available")
-@lru_with_ttl(ttl_seconds=60*10)
+# @lru_with_ttl(ttl_seconds=60*10)
 @transactional("read")
 def available_recipes(tx: ManagedTransaction, req: AvailableRecipeRequest):
+    print(req)
     result = tx.run("""
-WITH $ingredients AS ingredients
-match (r: Recipe) <-[:INGREDIENT_OF]- (i: Ingredient)
-where i.name in ingredients
-with r, r.name as recipeName, collect(i.name) as included
-with *, collect {
-  match (sr: Recipe) <-[:INGREDIENT_OF]- (si: Ingredient)
-  where elementId(r) = elementId(sr) and not (si.name in included)
-  return distinct si.name
-} as complementary
-return recipeName, included, complementary
-order by size(included) desc, size(complementary) asc
+WITH $ingredients as A
+MATCH (i)-[]->(r:Recipe)-[:RECIPE_OF]->(f:Food)
+with A, f, r, collect(i.name) as R 
+with *, apoc.coll.intersection(A, R) as X
+where size(X) <> 0
+with *, size(apoc.coll.intersection(X, R)) as i, size(apoc.coll.union(X, R)) as u
+where u <> 0
+with f.name as food, r.name as recipe, i/toFloat(u) as sim
+return food, recipe, sim order by sim desc
 """, ingredients=req.ingredients)
     values = result.values()
     print(values)
--- saveplate/model.py
+++ saveplate/model.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel
 
 class AvailableRecipeRequest(BaseModel):
     ingredients: list[str]
-    suaces: list[str]
+    sauces: list[str]
