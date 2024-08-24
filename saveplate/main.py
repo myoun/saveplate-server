3d367014496771d3a624ed09658bc88be026fff4
myoun <aiden080605@gmail.com>
2024-08-24 18:59:36 +0900

    feat: user update

--- saveplate/main.py
+++ saveplate/main.py
@@ -5,7 +5,7 @@ from saveplate.database import ManagedTransaction, transactional
 from saveplate.util import lru_with_ttl
 from typing import Literal
 from pydantic_settings import BaseSettings
-from saveplate.model import AvailableRecipeRequest
+from saveplate.model import AvailableRecipeRequest, AddUserIngredient
 
 class Settings(BaseSettings):
     DB_URL: str
@@ -66,5 +66,51 @@ with f.name as food, r.name as recipe, i/toFloat(u) as sim
 return food, recipe, sim order by sim desc
 """, ingredients=req.ingredients)
     values = result.values()
-    print(values)
     return values
+
+@app.get("/user/ingredients")
+@transactional("read")
+def my_ingredients(tx: ManagedTransaction, user_email: str):
+    result = tx.run("""
+        match (u: User) -[:HAS]-> (i) where u.email=$user_email return i
+    """, user_email=user_email)
+
+    values = result.values()
+
+    r = []
+    for e in values:
+        r.append(e[0])
+
+    return r
+
+@app.post("/user/ingredient")
+@transactional("write")
+def add_ingredient(tx: ManagedTransaction, req: AddUserIngredient):
+    user_email = req.user_email
+    ingredients = req.ingredients
+    user_result = tx.run("""
+        unwind $ingredients as e
+        match (u: User), (i)
+        where u.email = $user_email and i.name = e.name
+        merge p=(u)-[:Has{amount: e.amount}]->(i)
+        return u, i
+    """, user_email=user_email, ingrdients=ingredients)
+    return user_result.values()
+
+# @app.post("/user/ingredient")
+"""
+유저 이메일 -> 만들수 있는 레시피 구하기
+with "user001@email.com" as useremail
+match (u: User) where u.email = useremail
+match (u) -[:HAS]-> (i)
+where labels(i)[0] in ['Ingredient', 'Sauce']
+with collect(i.name) as A
+MATCH (i)-[]->(r:Recipe)-[:RECIPE_OF]->(f:Food)
+with A, f, r, collect(i.name) as R 
+with *, apoc.coll.intersection(A, R) as X
+where size(X) <> 0
+with *, size(apoc.coll.intersection(X, R)) as i, size(apoc.coll.union(X, R)) as u
+where u <> 0
+with f.name as food, r.name as recipe, i/toFloat(u) as sim
+return food, recipe, sim order by sim desc
+"""
